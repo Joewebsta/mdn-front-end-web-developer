7bd5b62fa18ce90503a1fbaeaaacd145055a5f03
Joe Webster <joseph.scott.webster@gmail.com>
2020-06-24 10:39:31 -0400

    Complete image gallery assessment

--- javascript/02-building-blocks/assessment-image-gallery/main.js
+++ javascript/02-building-blocks/assessment-image-gallery/main.js
@@ -1,13 +1,27 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
+const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
 
-/* Looping through images */
+images.forEach(image => {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${image}`);
+  thumbBar.appendChild(newImage);
+})
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+thumbBar.addEventListener('click', e => {
+  displayedImage.src = e.target.src;
+})
 
-/* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+  if (btn.classList.value === 'dark'){
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+  } else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = "rgba(0,0,0,0)";
+  }
+})
